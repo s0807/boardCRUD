07c3d7491235bed98d3d11acc3f208e0edf1b7d2
s0807 <111734755+s0807@users.noreply.github.com>
2022-12-26 15:52:32 +0900

    Add files via upload

---
+++ src/main/java/AteamUserVO.java
@@ -0,0 +1,80 @@
+package vo;
+
+public class AteamUserVO {
+	private String id;
+	private String password;
+	private String name;
+	private String tel;
+	private String email;
+	private String agree_clauses;
+	private String agree_info;
+	private String agree_email;
+	
+	public AteamUserVO() {}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAgree_clauses() {
+		return agree_clauses;
+	}
+
+	public void setAgree_clauses(String agree_clauses) {
+		this.agree_clauses = agree_clauses;
+	}
+
+	public String getAgree_info() {
+		return agree_info;
+	}
+
+	public void setAgree_info(String agree_info) {
+		this.agree_info = agree_info;
+	}
+
+	public String getAgree_email() {
+		return agree_email;
+	}
+
+	public void setAgree_email(String agree_email) {
+		this.agree_email = agree_email;
+	}
+	
+	
+}
